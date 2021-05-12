03805af7bbabf0ff033fd8585143b3bc98490902
RL2020 <loc.fitness1@gmail.com>
2021-05-12 17:14:41 -0400

    added functionality to employee index

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,30 @@
 class EmployeesController < ApplicationController
+
+    before_action :find_employee, only: [:show]
+
+    def index
+        if params[:user_id]
+            find_user
+            @employees = @user.employees
+        else 
+         @employees = Employee.all
+        end 
+    end 
+
+    def show 
+    end 
+
+    private
+
+    def find_employee
+        @employee = Employee.find_by_id(params[:id])
+    end 
+
+    def find_user
+        @user = User.find_by_id(params[:user_id])
+    end 
+
+    def employee_params
+        params.require(:employee).permit(:firs_name, :last_name, :title, :user_id, :office_id)
+    end 
 end
--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -1,2 +1,21 @@
 class OfficesController < ApplicationController
+
+    before_action :find_office, only: [:show]
+
+    def index
+        @offices = Office.all
+    end 
+    def show
+    end 
+
+
+    private
+
+    def find_office
+        @office = Office.find_by_id(params[:id])
+    end 
+
+    def employee_params
+        params.require(:office).permit(:company_name, :location, :owner)
+    end 
 end
