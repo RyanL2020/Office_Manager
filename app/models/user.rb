a6703b65a3882fe2b797ed9097482055ed7b1285
RL2020 <loc.fitness1@gmail.com>
2021-05-14 17:57:19 -0400

    Login functions, renamed 2 columns in users_table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
     has_secure_password 
     has_many :employees 
     has_many :offices, through: :employees
+    validates :username, presence: true, uniqueness: true  
 end
